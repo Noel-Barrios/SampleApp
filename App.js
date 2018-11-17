22603fcd91d7f63ee4bea67bb269ce3abde039fd
Noel Barrios <noel.barrios@optum.com>
2018-11-17 15:13:27 -0600

    add styling sheets

--- App.js
+++ App.js
@@ -1,62 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
-// import React, {Component} from 'react';
-// import {Platform, StyleSheet, Text, View} from 'react-native';
-//
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
-//
-// type Props = {};
-// export default class App extends Component<Props> {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>Welcome to React Native!</Text>
-//         <Text style={styles.instructions}>To get started, edit App.js</Text>
-//         <Text style={styles.instructions}>{instructions}</Text>
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-
-
-
-
-// ********************  Commented out above code; Lets build a React Native app from scratch  **********************
+// ********************  Lets build a React Native app from scratch  **********************
 
 // import ui components (the View and Text components) from react native
 // Here we are using an ES 6 destructuring statment in conjunction with an import statement
 // instead of importing the entire library, we will use curly brackets to import just these two components
-import { View, Text, AppRegistry, StatusBar } from 'react-native'
+import { StyleSheet, View, Text, AppRegistry, StatusBar } from 'react-native'
 
 // import the entire react library into a variable called 'React'
 import React from 'react'
@@ -75,13 +23,31 @@ export default class App extends React.Component {
     return (
       <View>
         <StatusBar hidden={true} />
-        <Text>Sierra</Text>
-        <Text>Tanner</Text>
-        <Text>Travis</Text>
+        <Text style={styles.defaultText}>Sierra</Text>
+        <Text style={[styles.defaultText,styles.selectedText]}>Tanner</Text>
+        <Text style={styles.defaultText}>Travis</Text>
       </View>
     );
   }
 }
+
+
+
+const styles = StyleSheet.create({
+  defaultText: {
+    fontSize: 100,
+    padding: 10,
+    margin: 5,
+    borderWidth: StyleSheet.hairlineWidth,
+    color: 'black',
+    textAlign: 'center',
+  },
+  selectedText: {
+    backgroundColor: 'yellow',
+    color: 'blue',
+    fontWeight: 'bold',
+  },
+});
 
 // first argument is the name of the application that we are registering the component for
 // the second argument is an arrow function which returns the "App" component.
